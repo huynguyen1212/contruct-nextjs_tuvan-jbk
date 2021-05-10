f61ee228bc3ecbc254afe7be043f3ed57e0e1f08
huynguyen1212 <huyro1212000@gmail.com>
2021-05-10 11:52:32 +0700

    config image apollo

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+//font
+declare module '*.ttf'
+declare module '*.woff2'
+//images
+declare module '*.svg'
+declare module '*.png'
--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,3 +1,10 @@
+// import addCircleIcon from '../../assets/images/add-circle-icon.svg'
+import IWithdr from 'assets/images/banner-img.png'
+
 export default function Home() {
-  return <div>home</div>
+  return (
+    <div>
+      <img src={IWithdr} alt="" />
+    </div>
+  )
 }
